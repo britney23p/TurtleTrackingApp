3f63f96375c75ba8a48ba7cd70bbe8e820fd088d
britney23p <97150103+britney23p@users.noreply.github.com>
2022-10-04 14:44:03 -0400

    Revert "Read all lines of data using a while loop"
    This reverts commit 7e71fc72ea86d3352ca10a5e19c3dda5e367d7f6.

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,15 +16,16 @@ file_name = 'data/raw/sara.txt'
 file_object = open(file = file_name,mode='r')
 
 #Read contents of file into a list
-lineString = file_object.readline()
+line_list = file_object.readlines()
 
+#Close the file object
+file_object.close()
 
 #Extract one data line into a variable
-while lineString:
+for lineString in line_list:
     
     #Check to see if the lineString is a data line
     if lineString[0] == "#" or lineString[0] == 'u':
-        lineString = file_object.readline()
         continue
     #Can Also use
     # if lineString[0] in ('#','u'):
@@ -45,9 +46,3 @@ while lineString:
     
     # Print information to the use
     print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
-
-    #move to the next line in the file
-    lineString = file_object.readline()
-
-#Close the file object
-file_object.close()
