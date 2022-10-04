8d79011a2cb88cb0901ce18af2d8837929472fad
britney23p <97150103+britney23p@users.noreply.github.com>
2022-10-04 15:32:55 -0400

    Filtering records to dictionaries

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -21,11 +21,15 @@ line_list = file_object.readlines()
 #Close the file object
 file_object.close()
 
+#Create empty dictionaires 
+date_dict = {}
+location_dict = {}
+
 #Extract one data line into a variable
 for lineString in line_list:
     
     #Check to see if the lineString is a data line
-    if lineString[0] == "#" or lineString[0] == 'u':
+    if lineString[0] == "#" or lineString[0] == "u":
         continue
     #Can Also use
     # if lineString[0] in ('#','u'):
@@ -40,9 +44,15 @@ for lineString in line_list:
     # Assign variables to specfic items in the list
     record_id = lineData[0]   # ARGOS tracking record ID
     obs_date = lineData[2]   # Observation date
-    ob_lc = lineData[4]       # Observation Location Class
+    obs_lc = lineData[4]       # Observation Location Class
     obs_lat = lineData[6]     # Observation Latitude
     obs_lon = lineData[7]     # Observation Longitude
     
+    #Add items to dictionaries, if lc criteria is met
+    if obs_lc in ('1', '2', '3'): 
+        #Add items to dictionaires
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat, obs_lon)
+    
     # Print information to the use
-    print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+    #print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
